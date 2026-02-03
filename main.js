1097d10883ba429d58527da776ba2258b9487372
Aarthika <aarthika@gmail.com>
2026-02-03 12:31:46 +0530

    first commi21

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // main.js
 import { factorial } from "./mathModule.js";
 
-console.log("5! =", factorial(5));   // 120
-console.log("0! =", factorial(0));   // 1
-console.log("-3! =", factorial(-3)); // undefined
+console.log("5! =", factorial(5));
+console.log("0! =", factorial(0));  
+console.log("-3! =", factorial(-3)); 
